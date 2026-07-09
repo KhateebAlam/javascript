6e3d1fd2670c4f4efa260fbfbc963045d50d4386
Khateeb Alam <KhateebAlam04@gmail.com>
2026-07-09 10:40:41 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict";//treat all JS code as newer version
+
+// alert(hello world) // we are using nodejs ,not browser
+
+console.log(3+3)
+
+console.log("sohail khan")
+
+let name = "sohail"
+let age = 22
+let isLoggedIn = false 
+let state;
+
+// number =>2 to power 53
+// bigInt 
+// string =>""
+// boolean =>true/false 
+// null =>standalone value
+// undefined =>
+// symbol    
+
+// object 
+console.log(typeof sohail); //string
+console.log(typeof null)  //object
+console.log(typeof undefined ); //undefined
+
+
