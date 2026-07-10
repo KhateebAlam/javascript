297dfb3ba1c551c9f1240c3bb8318367decfad9a
Khateeb Alam <KhateebAlam04@gmail.com>
2026-07-10 14:14:36 +0000

    conversion

--- 01_basic/conversionOP.js
+++ 01_basic/conversionOP.js
@@ -23,3 +23,17 @@ let stringNumber =String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
  
+// ***************operator************************
+console.log(+true);
+console.log(+"");
+
+let num1 ,num2 ,num3
+
+num1= num2 =num3 =2+2
+
+let gameCounter =100
+++gameCounter;
+console.console.log(gameCounter);
+
+// link to study 
+// https://tc39.es/ecma262/#sec-type-conversion
