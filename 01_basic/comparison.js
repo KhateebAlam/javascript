836853868a6a91bd3c73e21d565bb6264f226f88
Khateeb Alam <KhateebAlam04@gmail.com>
2026-07-10 14:43:31 +0000

    comparison

---
+++ 01_basic/comparison.js
@@ -0,0 +1,15 @@
+// console.log(2> 1);
+// console.log(2=>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2 != 1);
+
+console.log("2"> 1);
+console.log("02" >1);       
+console.log(null >0);   
+console.log(null == 0 );       
+console.log(null >= 0);       
+
+// === //strictly comparison 
+// it is also check data type
+console.log("02"===2);                       
