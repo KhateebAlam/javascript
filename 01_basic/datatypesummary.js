33070bef1c6325c7fc170bdf3460a51c888e2db8
Khateeb Alam <KhateebAlam04@gmail.com>
2026-07-10 17:13:02 +0000

    datatype

---
+++ 01_basic/datatypesummary.js
@@ -0,0 +1,37 @@
+// primitive / it is call by value datatype 
+
+// 7 type : string , Number ,Boolean ,null, undefined, symbol, BigInt 
+
+// JavaScript is a dynamic programming language
+
+ const score = 100
+ const scoreValue = 100.3
+
+const isLoggedIn = false 
+ const outsideTemp =null
+ let userEmail;
+
+ const id = symbol('123')
+ const anotherId = symbol('123')
+
+ console.log(id === anotherId);
+
+
+ const BigInt = 7142383179n
+                              
+// reference (Non primitive)
+
+// Array ,Object ,functions
+
+const heros = ["sohail","khan", "alam"] //array
+
+let myObj = {             //anything inside the curly braces is called object
+    name : "sohail",
+    age : 22,
+}
+
+// function 
+const myfunction = function(){
+    console.lo("hello world "); 
+
+}
