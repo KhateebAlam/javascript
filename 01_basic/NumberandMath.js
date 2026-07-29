25f2e61673e0d582a6579a779a45aa91a3b0f09e
Khateeb Alam <KhateebAlam04@gmail.com>
2026-07-29 12:46:49 +0000

    numberandmath

---
+++ 01_basic/NumberandMath.js
@@ -0,0 +1,19 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.734
+// console.log(otherNumber.toPrecision(4));
+
+//++++++++++++++++++ Math ++++++++++++++++++++++++++++++
+console.log(Math.floor(Math.random()*10)+ 1 );
+
+const min = 10 
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max -min +1)) +min)
