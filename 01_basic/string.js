da02866fc677ebfe9bd2569e714ca777fe4a5181
Khateeb Alam <KhateebAlam04@gmail.com>
2026-07-29 12:47:00 +0000

    string

---
+++ 01_basic/string.js
@@ -0,0 +1,16 @@
+const  name = "sohail"
+const repoCount = 50 
+
+// console.log(name + repoCount + "Value")
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('sohail')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+
