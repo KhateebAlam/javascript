3e15a0ccb9928b9324083b93dfb14c07c1ccd928
Khateeb Alam <KhateebAlam04@gmail.com>
2026-08-04 11:02:19 +0000

    function

---
+++ 03_basic/02_scopes.js
@@ -0,0 +1,17 @@
+ let a = 10 ;
+// const b = 20  ;
+// var c  = 30 ;
+
+if (true ){
+  let a = 10 ;
+const b = 20  ;
+
+}
+
+for (let i = 0; i < 10; i++){
+  
+}
+
+console.log(a);
+// console.log(b);
+
