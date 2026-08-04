69fbd070ce0a81062c1dfb2df00f66cbf1c161d5
Khateeb Alam <KhateebAlam04@gmail.com>
2026-08-04 11:23:09 +0000

    scopes

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -9,9 +9,54 @@ const b = 20  ;
 }
 
 for (let i = 0; i < 10; i++){
-  
+
 }
 
 console.log(a);
 // console.log(b);
 
+
+
+function one (){
+  const username = "sohail" ;
+   
+  function two (){
+    const website = "instagram ";
+    console.log(website);
+    console.log(username);
+  }
+  //console.log(website);
+  
+    two()
+      
+  }
+one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+                           
