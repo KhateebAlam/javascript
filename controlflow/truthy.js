2aae48895ebb7b5758cd590619a227c2f8498233
Khateeb Alam <KhateebAlam04@gmail.com>
2026-08-06 08:48:04 +0000

    NULLish coalescing Opt

--- controlflow/truthy.js
+++ controlflow/truthy.js
@@ -11,3 +11,29 @@ else{
 
 //false values: false,0,-0, BigInt 0n, undefined, "", null, undefined, NaN
              
+//truthy values:
+//"0", 'false', [], {}, "  ", function(){}
+
+if (userEmail.length === 0){
+  console.log("Array is empty");
+  
+}
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0){
+  console.log("Object is empty");
+}
+      
+//NULLish Coalescing Operator (??) : null undefined
+ 
+let val1 ;
+val1 = 5 ?? 10
+let val2 ;
+val2 = null ?? 10
+
+val3 = undefined ?? 178;
+
+console.log (val1);                         
+console.log (val2); 
+console.log (val3);                       
