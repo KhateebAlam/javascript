fd596ee39a05fc3a33e9ee6d8ae295c99af3247e
Khateeb Alam <KhateebAlam04@gmail.com>
2026-08-06 08:21:38 +0000

    controlflow

---
+++ controlflow/truthy.js
@@ -0,0 +1,13 @@
+const userEmail = []
+
+if (userEmail){
+  console.log("Got user email ")
+
+}
+else{
+  console.log("don't have user email");
+  
+} 
+
+//false values: false,0,-0, BigInt 0n, undefined, "", null, undefined, NaN
+             
