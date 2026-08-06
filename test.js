d52d364e9c1da9251ee8deeac98ef81ef5c0f0a6
Khateeb Alam <KhateebAlam04@gmail.com>
2026-08-06 07:35:28 +0000

    global and function excution

---
+++ test.js
@@ -0,0 +1,9 @@
+let val1 = 19;
+let val2 = 3;
+
+function addTwoNumbers(a, b){
+  let total = a + b;
+  return total; 
+}
+console.log(addTwoNumbers(val1, val2));
+console.log(addTwoNumbers(5, 10));
